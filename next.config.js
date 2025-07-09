4dff2aa884b7473486e6129dfb813864a8d8c4e7
mdabidind <booksofkid@gmail.com>
2025-07-10 02:11:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const timestamp = new Date().toISOString().replace(/[-:T]/g, '').slice(0, 12); // e.g., 202507091610
+
 module.exports = {
   output: 'export',
+  distDir: 'out-' + timestamp, // dynamically name folder
   trailingSlash: true,
 };
